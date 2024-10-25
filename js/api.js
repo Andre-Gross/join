e7b460e1d8974e279df114ecd899e40e5e966a2c
Andre-Gross <andre.handy94@gmail.com>
2024-10-25 22:48:44 +0200

    started addTask.html and .css; make some general styles for body, button, input and textarea; coded api.js

---
+++ js/api.js
@@ -0,0 +1,23 @@
+let BASE_URL = 'https://join-5b9f0-default-rtdb.europe-west1.firebasedatabase.app/';
+let loggedInUser = '1';
+
+
+async function loadData() {
+    let response = await fetch(BASE_URL + '.json');
+    let responseAsJSON = await response.json();
+    console.log(responseAsJSON.users['1'].login.locked);
+    return responseAsJSON.users['1'].login.locked;
+}
+
+
+// async function loadData() {
+//     let response = await fetch(BASE_URL + '.json');
+//     let responseAsJSON = await response.json();
+//     if (!responseAsJSON.users['1'].login.locked) {
+//        return responseAsJSON.users['1'].login.locked; 
+//     } else {
+//         alert('Bitte loggen sie sich ein');
+//     }
+// }
+
+
