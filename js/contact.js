8087dde86281f610602ca7f8a1e2f65bef3fe13d
Relja <55156943+M1llenn1um@users.noreply.github.com>
2024-10-27 20:04:34 +0100

    update design contact
    draft of design change contact

---
+++ js/contact.js
@@ -0,0 +1,64 @@
+function contactMain() {
+  let contactMain = document.getElementById("idContactMain");
+  contactMain.innerHTML = "";
+  contactMain.innerHTML += getContactMain();
+}
+
+function getContactMain() {
+  return `
+<div id="idContactAlph">
+<p>A</p>
+<div id="idGreyLine"></div>
+<p>B</p>
+<div id="idGreyLine"></div>
+<p>C</p>
+<div id="idGreyLine"></div>
+<p>D</p>
+<div id="idGreyLine"></div>
+<p>E</p>
+<div id="idGreyLine"></div>
+<p>F</p>
+<div id="idGreyLine"></div>
+<p>G</p>
+<div id="idGreyLine"></div>
+<p>H</p>
+<div id="idGreyLine"></div>
+<p>J</p>
+<div id="idGreyLine"></div>
+<p>K</p>
+<div id="idGreyLine"></div>
+<p>L</p>
+<div id="idGreyLine"></div>
+<p>M</p>
+<div id="idGreyLine"></div>
+<p>N</p>
+<div id="idGreyLine"></div>
+<p>O</p>
+<div id="idGreyLine"></div>
+<p>P</p>
+<div id="idGreyLine"></div>
+<p>Q</p>
+<div id="idGreyLine"></div>
+<p>R</p>
+<div id="idGreyLine"></div>
+<p>S</p>
+<div id="idGreyLine"></div>
+<p>T</p>
+<div id="idGreyLine"></div>
+<p>U</p>
+<div id="idGreyLine"></div>
+<p>V</p>
+<div id="idGreyLine"></div>
+<p>W</p>
+<div id="idGreyLine"></div>
+<p>X</p>
+<div id="idGreyLine"></div>
+<p>Y</p>
+<div id="idGreyLine"></div>
+<p>Z</p>
+<div id="idGreyLine"></div>
+</div>
+<button>
+<img src="" alt="">
+</button>`;
+}
