d99a6a58fb4a8bbf2131269a4fb284e0006356ac
Kay <118884667+BadRix90@users.noreply.github.com>
2024-10-28 21:52:02 +0100

    Provides functions to load data from Firebase, including all data and specific user data by ID.

--- js/api.js
+++ js/api.js
@@ -1,11 +1,18 @@
 let BASE_URL = 'https://join-5b9f0-default-rtdb.europe-west1.firebasedatabase.app/';
-let loggedInUser = '1';
-
 
 async function loadData() {
     let response = await fetch(BASE_URL + '.json');
     let responseAsJSON = await response.json();
     return responseAsJSON;
+}
+
+
+// Funktion zum Laden eines Benutzers basierend auf der ID -->> NUR ZUM TESTEN!
+// kann bei Bedarf gelöscht werden
+async function loadUserData(userId) {
+    let response = await fetch(`${BASE_URL}users/${userId}.json`);
+    let userData = await response.json();
+    return userData;
 }
 
 
