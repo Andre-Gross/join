8e0462a852d590589f50b44961b5311d257ef912
Andre-Gross <andre.handy94@gmail.com>
2024-10-29 00:28:20 +0100

    added loadContacts()

--- js/api.js
+++ js/api.js
@@ -1,6 +1,5 @@
 let BASE_URL = 'https://join-5b9f0-default-rtdb.europe-west1.firebasedatabase.app/';
-let loggedInUser = '1';
-
+let loggedInUser = '2';
 
 async function loadData() {
     let response = await fetch(BASE_URL + '.json');
@@ -9,14 +8,8 @@ async function loadData() {
 }
 
 
-// async function loadData() {
-//     let response = await fetch(BASE_URL + '.json');
-//     let responseAsJSON = await response.json();
-//     if (!responseAsJSON.users['1'].login.locked) {
-//        return responseAsJSON.users['1'].login.locked; 
-//     } else {
-//         alert('Bitte loggen sie sich ein');
-//     }
-// }
-
-
+async function loadContacts() {
+    let response = await fetch(BASE_URL + 'users/' + loggedInUser + '/contacts' + '.json');
+    let responseAsJSON = await response.json();
+    return responseAsJSON;
+}
