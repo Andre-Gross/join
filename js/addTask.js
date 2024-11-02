a1f17d6627e1717b7cb1271652d04e10a0f93f58
Andre-Gross <andre.handy94@gmail.com>
2024-11-02 19:40:11 +0100

    renamed some functions in addTask.js and added documentation; replaced new function names in addTask.html

--- js/addTask.js
+++ js/addTask.js
@@ -1,6 +1,9 @@
 let contactsInitialized = false;
 
-
+/**
+ * This function filter your contacts with the input of addTasks and show the result in a dropdown menu.
+ * 
+ */
 async function filterContacts() {
     const input = document.getElementById("navAssignedTo").value.toLowerCase();
     const dropdown = document.getElementById("contactDropdown");
@@ -9,7 +12,7 @@ async function filterContacts() {
     const filteredContacts = input ? contacts.filter(contact => contact.name.toLowerCase().includes(input)) : contacts;
 
     if (filteredContacts.length > 0) {
-        showContactsDropdown(); // Dropdown anzeigen
+        showAssignedToDropdown();
         for (i = 0; i < contacts.length; i++) {
             const contact = contacts[i];
             const item = document.getElementById(`item_${contact.name.replace(/\s+/g, '_')}`)
@@ -23,18 +26,48 @@ async function filterContacts() {
             }
         }
     } else {
-        hideContactsDropdown(); // Dropdown ausblenden, wenn keine Kontakte übrig sind
+        hideAssignedToDropdown()();
     }
 }
 
 
-function selectContact(contact) {
-    let checkbox = document.getElementById(`checkbox_${contact.name.replace(/\s+/g, '_')}`)
-    checkbox.checked = !checkbox.checked;
+/**
+ * This function show the assignedTo-Dropdown-Menu.
+ * 
+ */
+async function showAssignedToDropdown() {
+    const dropdown = document.getElementById("contactDropdown");
+    const arrowDown = document.getElementById("arrowDown");
+    const arrowUp = document.getElementById("arrowUp");
+
+    if (!contactsInitialized) {
+        await createAssignedToDropdown(); // Erstelle das Dropdown, wenn es noch nicht initialisiert wurde
+    }
+
+    dropdown.classList.remove("d-none"); // Zeige das Dropdown an
+    arrowDown.classList.add("d-none");
+    arrowUp.classList.remove("d-none");
 }
 
 
-async function createContactsDropdown() {
+/**
+ * This function hide the assignedTo-Dropdown-Menu.
+ */
+function hideAssignedToDropdown() {
+    const dropdown = document.getElementById("contactDropdown");
+    const arrowDown = document.getElementById("arrowDown");
+    const arrowUp = document.getElementById("arrowUp");
+
+    dropdown.classList.add("d-none"); // Blende das Dropdown aus
+    arrowDown.classList.remove("d-none"); // Zeige den nach unten zeigenden Pfeil
+    arrowUp.classList.add("d-none"); // Verstecke den nach oben zeigenden Pfeil
+}
+
+
+/**
+ * This function create the assignedTo-Dropdown-Menu, if is not already initialized.
+ */
+async function createAssignedToDropdown() {
     const dropdown = document.getElementById("contactDropdown");
     let contacts = await loadContacts();
 
@@ -53,85 +86,75 @@ async function createContactsDropdown() {
 
         contactItem.onclick = () => selectContact(contact);
 
-        const checkbox = contactItem.querySelector(`#${checkboxId}`);
-        checkbox.onclick = (event) => {
-            // Verhindere das Auslösen des onclick-Handlers des contactItem
-            event.stopPropagation();
-        };
-
         dropdown.appendChild(contactItem);
     });
 
     contactsInitialized = true; // Setze das Flag auf true, nachdem die Kontakte initialisiert wurden
 }
 
-// Funktion zur Anzeige des Dropdown-Menüs
-async function showContactsDropdown() {
-    const dropdown = document.getElementById("contactDropdown");
-    const arrowDown = document.getElementById("arrowDown");
-    const arrowUp = document.getElementById("arrowUp");
 
-    if (!contactsInitialized) {
-        await createContactsDropdown(); // Erstelle das Dropdown, wenn es noch nicht initialisiert wurde
-    }
+/**
+ * This function allow to select the checkbox of an contact in the assingedTo-Dropdown-Menu by clicking on the contact (not necessary to click specificly on the checkbox)
+ * 
+ *  @param {object} contact - This object includes the information of a single contact.
+ */
+function selectContact(contact) {
+    let checkbox = document.getElementById(`checkbox_${contact.name.replace(/\s+/g, '_')}`)
+    checkbox.checked = !checkbox.checked;
 
-    dropdown.classList.remove("d-none"); // Zeige das Dropdown an
-    arrowDown.classList.add("d-none");
-    arrowUp.classList.remove("d-none");
+    checkbox.onclick = (event) => {
+        event.stopPropagation();
+    };
 }
 
 
-function hideContactsDropdown() {
-    const dropdown = document.getElementById("contactDropdown");
-    const arrowDown = document.getElementById("arrowDown");
-    const arrowUp = document.getElementById("arrowUp");
-
-    dropdown.classList.add("d-none"); // Blende das Dropdown aus
-    arrowDown.classList.remove("d-none"); // Zeige den nach unten zeigenden Pfeil
-    arrowUp.classList.add("d-none"); // Verstecke den nach oben zeigenden Pfeil
-}
-
-
+/**
+ * This function allow to select and change the priority-button.
+ * 
+ * @param {string} priority - That includes the name of the selected priority.
+ */
 function selectPriority(priority) {
-    // Entferne die aktive Klasse von allen Schaltflächen
     document.getElementById("urgent").classList.remove("btn-selected");
     document.getElementById("medium").classList.remove("btn-selected");
     document.getElementById("low").classList.remove("btn-selected");
 
-    // Füge die aktive Klasse zur ausgewählten Schaltfläche hinzu
     document.getElementById(priority).classList.add("btn-selected");
 }
 
-// Funktion, um den aktuellen Prioritätswert zu erhalten
+
+/**
+ * That function return the priority of the choosen button.
+ * 
+ * @returns - the priority of the choosen button
+ */
 function getSelectedPriority() {
     if (document.getElementById("urgent").classList.contains("btn-selected")) return "urgent";
     if (document.getElementById("medium").classList.contains("btn-selected")) return "medium";
     if (document.getElementById("low").classList.contains("btn-selected")) return "low";
-    return null; // Wenn keine Schaltfläche ausgewählt ist
 }
 
 
+/**
+ * This function prepare the information of the addTask-form and post it to the database.
+ * 
+ * @param {string} boardId - That includes the ID of the board, where the datas should be post.
+ */
 async function submitTaskForm(boardId) {
-    // Hole alle Eingabefelder
     const title = document.getElementById("inputTitle").value.trim();
     const description = document.getElementById("textareaDescription").value.trim();
     const dueDate = document.getElementById('inputDate').value;
     const priority = document.querySelector('.btn-selected')?.id;
 
-    // Checkboxen für "assignedTo" auslesen
     const assignedTo = readAssignedTo();
 
-    // Überprüfe, ob alle Felder ausgefüllt sind
     if (checkAllInputsHasContent(title, description, dueDate, priority, assignedTo)) {
-        // Datenstruktur für das Posten in die Datenbank vorbereiten
         const data = {
             title: title,
             description: description,
             finishedUntil: dueDate,
             priority: priority,
-            assignedTo: assignedTo // Hier die IDs der zugewiesenen Kontakte hinzufügen
+            assignedTo: assignedTo 
         };
-        // Anfrage zum Posten der Daten in die Datenbank
         await postTaskToDatabase(boardId, data);
 
         emptyAddTaskInputs();
@@ -139,6 +162,11 @@ async function submitTaskForm(boardId) {
 }
 
 
+/**
+ * That function read the information, for which contact the task is assigned to.
+ * 
+ * @returns - The person(s), which are assigned to to do the task.
+ */
 function readAssignedTo() {
     const assignedToCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
     const assignedTo = Array.from(assignedToCheckboxes).map(checkbox => checkbox.id); // IDs der ausgewählten Checkboxen
@@ -146,6 +174,16 @@ function readAssignedTo() {
 }
 
 
+/**
+ * This function, check if all inputs of the addTask-form includes content.
+ * 
+ * @param {string} title - Title of the task
+ * @param {string} description - description of the task
+ * @param {Date} dueDate - Date, until the task should be finished
+ * @param {string} priority - Priority, how important the task is
+ * @param {Array} assignedTo - The person(s), who are assigned to do the task
+ * @returns 
+ */
 function checkAllInputsHasContent(title, description, dueDate, priority, assignedTo) {
     if (title == '' || description == '' || dueDate == '' || priority == '' || assignedTo.length == 0) {
         alert("Bitte fülle alle Felder aus.");
@@ -156,9 +194,13 @@ function checkAllInputsHasContent(title, description, dueDate, priority, assigne
 }
 
 
+/**
+ * This function reset the form of addTask
+ */
 function emptyAddTaskInputs() {
-    const title = document.getElementById("iTitle").value = '';
-    const description = document.getElementById("taDescription").value = '';
-    const dueDate = document.getElementById('iDate').value = '';
-    const priority = document.querySelector('.btn-selected')?.classList.remove('btn-selected');
+    document.getElementById("iTitle").value = '';
+    document.getElementById("taDescription").value = '';
+    document.getElementById('iDate').value = '';
+    document.querySelector('.btn-selected')?.classList.remove('btn-selected');
+    document.document.getElementById('medium').classList.add('btn-selected');
 }
