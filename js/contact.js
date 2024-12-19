e6580dd5b2541365cd288c369e6cdbca807578e6
Kay <118884667+BadRix90@users.noreply.github.com>
2024-12-20 00:57:16 +0100

    rmv function
    funktion entfernt. es wird meine genutzt aus der api

--- js/contact.js
+++ js/contact.js
@@ -224,13 +224,9 @@ function saveEditContact(i) {
 }
 
 async function shortName() {
-  shortNames = names.map(name => {
-    let parts = name.split(" "); 
-    let firstInitial = parts[0][0].toUpperCase();
-    let lastInitial = parts[1] ? parts[1][0].toUpperCase() : ""; 
-    return firstInitial + lastInitial; 
-  });
+  shortNames = names.map(name => getInitialsFromName(name));
 }
+
 
 
 async function postContactToDatabase(name, mail, phone, color) {
