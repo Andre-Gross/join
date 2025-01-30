f133e407c9057bbc1f9ef5e9c6d6bec93dbc9dca
Kay <118884667+BadRix90@users.noreply.github.com>
2025-01-30 22:48:00 +0100

    update function for check loggin user

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    checkAuth(); 
+    checkAuth();
 });
 
 /**
  * Prüft, ob der Benutzer eingeloggt ist. Falls nicht, wird er zur Login-Seite weitergeleitet.
+ * Ausnahme: Wenn der Benutzer als "Guest" eingeloggt ist, bleibt er auf der Seite.
  */
 function checkAuth() {
-    const loggedInUser = localStorage.getItem("loggedInUser");
+    const loggedInUserId = localStorage.getItem("loggedInUserId"); // Holen der Benutzer-ID
 
+    if (!loggedInUserId || loggedInUserId === "null" || loggedInUserId === "undefined") {
+        window.location.href = "index.html";
+        return;
+    }
+
+    if (loggedInUserId.toLowerCase() === "guest") {
+        return; // Falls der Nutzer als "Guest" eingeloggt ist, bleibt er auf der Seite
+    }
+
+    // Falls kein gültiger Benutzer gefunden wurde, zur Login-Seite weiterleiten
+    const loggedInUser = localStorage.getItem("loggedInUser");
     if (!loggedInUser) {
         window.location.href = "index.html";
     }
 }
+
+
+
